f368531481045e78cecc0f811167b4571fcb32a3
Israel Alves <israel_alves77@hotmail.com>
2026-02-18 16:52:06 -0300

    Implementar classe de data access object
    https://github.com/Khaldewey/java-web-sergipe-tec-challenge/issues/2

---
+++ src/main/java/br/com/produtos/dao/ProdutoDAO.java
@@ -0,0 +1,25 @@
+package br.com.produtos.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import br.com.produtos.config.ConnectionFactory;
+import br.com.produtos.models.Produto;
+
+public class ProdutoDAO {
+    
+    public void salvar(Produto produto) throws Exception {
+    String sql = "INSERT INTO produtos (descricao, valor, estoque, datacadastro) VALUES (?, ?, ?, ?)";
+
+    try (Connection conn = ConnectionFactory.getConnection();
+         PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+        stmt.setString(1, produto.getDescricao());
+        stmt.setBigDecimal(2, produto.getValor());
+        stmt.setInt(3, produto.getQuantidadeEmEstoque());
+        stmt.setDate(4, produto.getDataDeCadastro());
+        stmt.executeUpdate();
+    }
+}
+
+}
