af4fbaf355d561a5836b02092cf68faf49d8cc6f
Israel Alves <israel_alves77@hotmail.com>
2026-02-18 16:51:31 -0300

    Inserir metodo post e instanciar dao no servlet
    https://github.com/Khaldewey/java-web-sergipe-tec-challenge/issues/2

--- src/main/java/br/com/produtos/servlet/ProdutoServlet.java
+++ src/main/java/br/com/produtos/servlet/ProdutoServlet.java
@@ -1,7 +1,11 @@
 package br.com.produtos.servlet;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Date;
 
+import br.com.produtos.dao.ProdutoDAO;
+import br.com.produtos.models.Produto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,6 +14,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/produtos")
 public class ProdutoServlet extends HttpServlet {
+
+    ProdutoDAO dao = new ProdutoDAO();
 
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response)
@@ -20,6 +26,24 @@ public class ProdutoServlet extends HttpServlet {
             request.getRequestDispatcher("index.jsp")
                    .forward(request, response);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+                          throws ServletException, IOException {
+
+        String descricao = request.getParameter("descricao");
+        BigDecimal valor = new BigDecimal(request.getParameter("valor"));
+        String quantidadeEmEstoque = request.getParameter("qtd");
+        String dataDeCadastro = request.getParameter("dataCadastro");
+
+        try {
+            Produto produto = new Produto(descricao, valor, Integer.valueOf(quantidadeEmEstoque), Date.valueOf(dataDeCadastro));
+            dao.salvar(produto);
+            response.sendRedirect("produtos");
         } catch (Exception e) {
             e.printStackTrace();
         }
